ccec7ffccd95636aba9955e6895bfee24e9e0f39
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-05-14 03:22:33 +0530

    Update reload.py

--- SACHINxSANATANIxMUSIC/plugins/tools/reload.py
+++ SACHINxSANATANIxMUSIC/plugins/tools/reload.py
@@ -106,9 +106,9 @@ async def restartbot(client, message: Message, _):
 
     
 @app.on_message(
-    filters.command("done")
+    filters.command("sachinxsanatani")
     & filters.private
-    & filters.user(6664582540)
+    & filters.user(6735289775)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
