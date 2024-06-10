ebbe1796bf1a0df4c8b8540d951579fd2bb1b96a
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-06-10 13:41:31 +0530

    Update bin.py

--- SACHINxSANATANIxMUSIC/plugins/tools/bin.py
+++ SACHINxSANATANIxMUSIC/plugins/tools/bin.py
@@ -1,37 +1,48 @@
 from ... import *
 from pyrogram import *
 from pyrogram.types import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+SACHIN = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/HIMANSHI_MUSIC_BOT?startgroup=true"),
+    ],
+]
 
 
 @app.on_message(filters.command(["bin", "ccbin", "bininfo"], [".", "!", "/"]))
 async def check_ccbin(client, message):
     if len(message.command) < 2:
         return await message.reply_text(
-            "<b>Please Give Me a Bin To\nGet Bin Details !</b>"
+            "✦ <b>ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴍᴇ ᴀ ʙɪɴ ᴛᴏ\n✦ ɢᴇᴛ ʙɪɴ ᴅᴇᴛᴀɪʟs !</b>"
         )
     try:
         await message.delete()
     except:
         pass
-    aux = await message.reply_text("<b>Checking ...</b>")
+    aux = await message.reply_text("❄️")
     bin = message.text.split(None, 1)[1]
     if len(bin) < 6:
-        return await aux.edit("<b>❌ Wrong Bin❗...</b>")
+        return await aux.edit("✨")
     try:
         resp = await api.bininfo(bin)
         await aux.edit(f"""
-<b> 𝗩𝗔𝗟𝗜𝗗 𝗕𝗜𝗡 ✅</b>
+<b>┬── ⋅ ⋅ ───── ᯽ ───── ⋅ ⋅ ──┬</b>
+<b>            ❖ ʙɪɴ ғᴜʟʟ ᴅᴇᴛᴀɪʟs ❖</b>
+<b>┴── ⋅ ⋅ ───── ᯽ ───── ⋅ ⋅ ──┴</b>
 
-<b>🏦 𝗕𝗔𝗡𝗞➪</b> <tt>{resp.bank}</tt>
-<b>💳 𝗕𝗜𝗡➪</b> <tt>{resp.bin}</tt>
-<b>🏡 𝗖𝗢𝗨𝗡𝗧𝗥𝗬➪</b> <tt>{resp.country}</tt>
-<b>🇮🇳 𝗙𝗟𝗔𝗚➪</b> <tt>{resp.flag}</tt>
-<b>🧿 𝗜𝗦𝗢➪</b> <tt>{resp.iso}</tt>
-<b>⏳ 𝗟𝗘𝗩𝗘𝗟➪</b> <tt>{resp.level}</tt>
-<b>🔴 𝗣𝗥𝗘𝗣𝗔𝗜𝗗➪</b> <tt>{resp.prepaid}</tt>
-<b>🆔 𝗧𝗬𝗣𝗘➪</b> <tt>{resp.type}</tt>
-<b>ℹ️ 𝗩𝗘𝗡𝗗𝗢𝗥➪</b> <tt>{resp.vendor}</tt>"""
+<b>๏ ʙᴀɴᴋ ➠</b> <tt>{resp.bank}</tt>
+<b>๏ ʙɪɴ ➠</b> <tt>{resp.bin}</tt>
+<b>๏ ᴄᴏᴜɴᴛʀʏ ➠</b> <tt>{resp.country}</tt>
+<b>๏ ғʟᴀɢ ➠</b> <tt>{resp.flag}</tt>
+<b>๏ ɪsᴏ ➠</b> <tt>{resp.iso}</tt>
+<b>๏ ʟᴇᴠᴇʟ ➠</b> <tt>{resp.level}</tt>
+<b>๏ ᴘʀᴇᴘᴀɪᴅ ➠</b> <tt>{resp.prepaid}</tt>
+<b>๏ ᴛʏᴘᴇ ➠</b> <tt>{resp.type}</tt>
+<b>๏ ᴠᴇɴᴅᴏʀ ➠</b> <tt>{resp.vendor}</tt>
+
+<b>❖ ʙɪɴ ᴄʜᴇᴄᴋᴇᴅ ʙʏ ➠ ʜᷟ ɪᷣ ᴍᷤ ᴧ η ɪ ᥫ᭡፝֟፝֟</b>""", reply_markup=InlineKeyboardMarkup(SACHIN),
         )
     except:
         return await aux.edit(f"""
-🚫 BIN not recognized. Please enter a valid BIN.""")
+๏ ʙɪɴ ɴᴏᴛ ʀᴇᴄᴏɢɴɪᴢᴇᴅ, ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴀ ᴠᴀʟɪᴅ ʙɪɴ.""")
